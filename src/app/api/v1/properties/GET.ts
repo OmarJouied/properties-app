ab0b9cbabe449cd951fdf3e07a184ba745679590
OmarJouied <omarjouied1421@yahoo.com>
2024-08-29 18:43:48 +0100

    connect mysql database

--- src/app/api/v1/properties/GET.ts
+++ src/app/api/v1/properties/GET.ts
@@ -1,5 +1,16 @@
-const GET = () => {
-  return Response.json({ run: true }, { status: 200 })
+import pool from "@/lib/db";
+
+const GET = async () => {
+  try {
+    const db = await pool.getConnection();
+    const query = "SELECT * FROM properties";
+    const [rows] = await db.execute(query);
+    db.release();
+
+    return Response.json({ rows }, { status: 200 })
+  } catch (error) {
+    return Response.json({ error }, { status: 500 })
+  }
 }
 
 export default GET;
