b846ba7bb248547b1dc7c39cede5765fd7b52c99
OmarJouied <omarjouied1421@yahoo.com>
2024-08-31 19:55:18 +0100

    add get users endpoint

--- src/app/api/v1/users/GET.ts
+++ src/app/api/v1/users/GET.ts
@@ -1,3 +1,15 @@
-const GET = () => { }
+import pool from "@/lib/db";
+
+const GET = async () => {
+  try {
+    const db = await pool.getConnection();
+    const query = "SELECT * FROM users";
+    const [rows] = await db.execute(query);
+
+    return Response.json({ users: rows }, { status: 200 });
+  } catch (error: any) {
+    return Response.json({ error: true, msg: error.message }, { status: 500 });
+  }
+}
 
 export default GET;
